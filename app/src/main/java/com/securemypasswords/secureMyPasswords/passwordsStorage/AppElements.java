a5410504f4e534cee3337d92be9bcc192bc94006
Amosar <maxi.cass11@gmail.com>
2018-01-12 02:39:36 +0000

    correct deleted elements

--- app/src/main/java/com/securemypasswords/secureMyPasswords/passwordsStorage/AppElements.java
+++
@@ -1,9 +0,0 @@
-package com.securemypasswords.secureMyPasswords.passwordsStorage;
-
-import java.io.Serializable;
-
-public interface AppElements extends Serializable {
-
-    String getName();
-    void setName(String name);
-}
